3380196198b6c566631782b8b2ea7fce7d188664
Миронов Дмитрий <mironov.dv@molodost.bz>
2017-07-21 15:49:29 +0300

    Удалил старое

--- routes/new_game.js
+++
@@ -1,7 +0,0 @@
-var shortid = require('shortid');
-
-module.exports = require('express').Router().get('/', function (req, res) {
-    var gameId = shortid.generate();
-
-    res.redirect('/game/' + gameId + '/');
-});
